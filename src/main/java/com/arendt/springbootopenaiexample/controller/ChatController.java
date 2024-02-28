f0ee4eb7f7e918a9bef6dca1d4a1ca30b3a915d6
Aremju <julius.arendt@klosebrothers.de>
2024-02-28 19:23:29 +0100

    make generate method return String only

--- src/main/java/com/arendt/springbootopenaiexample/controller/ChatController.java
+++ src/main/java/com/arendt/springbootopenaiexample/controller/ChatController.java
@@ -1,15 +1,9 @@
 package com.arendt.springbootopenaiexample.controller;
 
-import org.springframework.ai.chat.ChatResponse;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-
-import java.util.Map;
 
 @RestController
 public class ChatController {
@@ -20,13 +14,7 @@ public class ChatController {
     }
 
     @GetMapping("/ai/generate")
-    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        return Map.of("generation", chatClient.call(message));
-    }
-
-    @GetMapping("/ai/generateStream")
-    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        Prompt prompt = new Prompt(new UserMessage(message));
-        return chatClient.stream(prompt);
+    public String generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return chatClient.call(message);
     }
 }
