93461c08e4395bed368660c5b7a18c0934065c70
Aremju <julius.arendt@klosebrothers.de>
2024-02-28 19:19:45 +0100

    add example chat controller

---
+++ src/main/java/com/arendt/springbootopenaiexample/controller/ChatController.java
@@ -0,0 +1,32 @@
+package com.arendt.springbootopenaiexample.controller;
+
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+public class ChatController {
+    private final OpenAiChatClient chatClient;
+
+    public ChatController(final OpenAiChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatClient.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatClient.stream(prompt);
+    }
+}
