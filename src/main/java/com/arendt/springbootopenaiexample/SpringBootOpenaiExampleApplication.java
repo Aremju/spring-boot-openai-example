7f3381adafe1b4e3c0a4c42b7a0d9dea149ed151
Aremju <julius.arendt@klosebrothers.de>
2024-02-28 19:11:43 +0100

    add example Rest Controller for Spring Boot and OpenAI

---
+++ src/main/java/com/arendt/springbootopenaiexample/SpringBootOpenaiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.arendt.springbootopenaiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenaiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOpenaiExampleApplication.class, args);
+    }
+
+}
